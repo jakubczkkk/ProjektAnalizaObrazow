2921b7652dfffce4dd336caefd6e441814cbbd1d
Rafal Jakubczyk <jakubczykrafal06@gmail.com>
2021-01-08 19:34:29 +0100

    test

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-# from skimage.util import img_as_float
 from skimage import io
 from skimage import color
 from skimage import morphology
@@ -6,17 +5,19 @@ from skimage import transform
 from skimage.filters import threshold_otsu
 import numpy as np
 
-kitku = color.rgb2gray(io.imread("kitku2.jpg"))
-kitku = transform.resize(kitku, (300, 300))
+def load_image(file_name):
 
-thresh = threshold_otsu(kitku)
-kitku = kitku > thresh
+    kitku = color.rgb2gray(io.imread(file_name))
+    kitku = transform.resize(kitku, (300, 300))
 
-kitku = morphology.binary_erosion(kitku)
-kitku = morphology.binary_dilation(kitku)
+    thresh = threshold_otsu(kitku)
+    kitku = kitku > thresh
 
-kitku = morphology.remove_small_objects(kitku, min_size=50)
-kitku = morphology.remove_small_holes(kitku, area_threshold=50)
+    kitku = morphology.binary_erosion(kitku)
+    kitku = morphology.binary_dilation(kitku)
 
-io.imshow(kitku)
-io.show()
+    kitku = morphology.remove_small_objects(kitku, min_size=50)
+    kitku = morphology.remove_small_holes(kitku, area_threshold=50)
+
+    io.imshow(kitku)
+    io.show()
